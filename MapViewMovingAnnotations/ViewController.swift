8e77bbc93c27db360fcd0a56e29952e4bab0385b
AinaraPerez <ainara.pa85@gmail.com>
2020-09-17 12:57:57 +0200

    Initial Commit

---
+++ MapViewMovingAnnotations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapViewMovingAnnotations
+//
+//  Created by Ainara Perez on 17/09/2020.
+//  Copyright © 2020 Ainara Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
